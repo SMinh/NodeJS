61a810952373f1853326fb44b0e24d79d247be81
Son Minh <minh.huynhcao@gmail.com>
2022-07-21 15:43:00 +0700

    lab5.11

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,6 +14,19 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description,
+  })
+    .then((result) => {
+      // console.log(result);
+      console.log("Create Product");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   product
     .save()
     .then(() => {
