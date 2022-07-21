3dd4ff4433b033bc96b6ae8ca50a2a1cc05232be
Son Minh <minh.huynhcao@gmail.com>
2022-07-21 21:55:55 +0700

    lab5.15

--- controllers/admin.js
+++ controllers/admin.js
@@ -41,17 +41,21 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId, (product) => {
-    if (!product) {
-      return res.redirect("/");
-    }
-    res.render("admin/edit-product", {
-      pageTitle: "Edit Product",
-      path: "/admin/edit-product",
-      editing: editMode,
-      product: product,
+  Product.findByPk(prodId)
+    .then((product) => {
+      if (!product) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  });
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -60,7 +64,20 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
+  Product.findByPk(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDesc;
+      return product.save();
+    })
+    .then((result) => res.redirect("/admin/products"))
+    .catch((err) => {
+      console.log(err);
+    });
+
+  /*const updatedProduct = new Product(
     prodId,
     updatedTitle,
     updatedImageUrl,
@@ -69,7 +86,7 @@ exports.postEditProduct = (req, res, next) => {
   );
   console.log(updatedProduct);
   updatedProduct.save();
-  res.redirect("/admin/products");
+  res.redirect("/admin/products");*/
 };
 
 exports.getProducts = (req, res, next) => {
