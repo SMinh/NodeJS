6059b0e49e3550e72e323f51062ca592b515bc80
Son Minh <minh.huynhcao@gmail.com>
2022-07-22 09:19:53 +0700

    lab5.19

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
   User.findByPk(1)
-    .then((uer) => {
+    .then((user) => {
       req.user = user;
       next();
     })
