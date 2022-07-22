558e11219f7225b444939e60e0737d5a33d43de9
Son Minh <minh.huynhcao@gmail.com>
2022-07-22 15:33:09 +0700

    lab5.28

--- controllers/shop.js
+++ controllers/shop.js
@@ -111,9 +111,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -128,6 +130,9 @@ exports.postOrder = (req, res, next) => {
           );
         })
         .then((result) => {
+          return fetchedCart.setProducts(null);
+        })
+        .then((result) => {
           res.redirect("/orders");
         })
         .catch((err) => {
@@ -138,15 +143,17 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    path: "/checkout",
-    pageTitle: "Checkout",
-  });
+  req.user
+    .getOrders({ include: ["products"] })
+    .then((orders) => {
+      console.log("orderssss", orders);
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
