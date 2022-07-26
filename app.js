40ad589675032badf419693509896e04a6d67846
Son Minh <minh.huynhcao@gmail.com>
2022-07-26 20:56:06 +0700

    lab7.1

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const path = require("path");
+const mongoose = require("mongoose");
 
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
 const User = require("./models/user");
 
 const app = express();
@@ -32,6 +32,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect("mongodb+srv://minhhc:2adicatooway@cluster0.n4a20.mongodb.net/test")
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
