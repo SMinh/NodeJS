958507bf77701216cd405683ade248f302f132f0
Son Minh <minh.huynhcao@gmail.com>
2022-07-26 20:32:09 +0700

    lab6.24

--- controllers/shop.js
+++ controllers/shop.js
@@ -120,9 +120,8 @@ exports.postOrder = (req, res, next) => {
 
 exports.getOrders = (req, res, next) => {
   req.user
-    .getOrders({ include: ["products"] })
+    .getOrders()
     .then((orders) => {
-      console.log("orderssss", orders);
       res.render("shop/orders", {
         path: "/orders",
         pageTitle: "Your Orders",
