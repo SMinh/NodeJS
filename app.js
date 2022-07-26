2cb9d3c0d9495202eb701a192683efd451f1b6e9
Son Minh <minh.huynhcao@gmail.com>
2022-07-26 21:44:52 +0700

    lab7.3

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const User = require("./models/user");
+// const User = require("./models/user");
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("62de598920a2b5239a3249ff")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("62de598920a2b5239a3249ff")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -33,7 +33,9 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect("mongodb+srv://minhhc:2adicatooway@cluster0.n4a20.mongodb.net/test")
+  .connect(
+    "mongodb+srv://minhhc:2adicatooway@cluster0.n4a20.mongodb.net/shop?retryWrites=true"
+  )
   .then((result) => {
     app.listen(3000);
   })
